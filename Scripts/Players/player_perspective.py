9ece3c3b55bebd0a1780ae9f9825f1caa6ad3968
Henry Ginn <henryginn137@gmail.com>
2025-10-13 14:40:29 +0100

    Add turn basic structure

--- Scripts/Players/player_perspective.py
+++ Scripts/Players/player_perspective.py
@@ -3,10 +3,9 @@ from Players.player import Player
 
 class PlayerPerspective(Player):
 
-    player_type = "Perspective"
-
     def __init__(self, perspective, base):
         name = f"{base.name} view {perspective.name}"
         self.view = f"{perspective.name}"
         super().__init__(base.catan, name)
         self.base = base
+        self.them = perspective
